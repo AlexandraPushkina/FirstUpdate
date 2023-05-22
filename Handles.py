a89514ea7621e530b391aa05da215e780cb59331
Alexandra <whitetomatoo@gmail.com>
2023-05-22 17:40:25 +1000

    New branch WithoutOOP

--- Handles.py
+++ Handles.py
@@ -1,47 +1,44 @@
 import tkinter
 from tkinter import *
-from Application_main import *
+import Application_main
 
-class Function(Helper):
-    onClickInsert = True
-    def __init__(self, root_out, onClickInsert):
-        super().__init__(root_out)
-        self.onClickInsert = onClickInsert
 
-    def handle_click_btnInsert(event): # Create function to handle btnInsert
+def clear_main_label():
 
-        def clear(): # function to remove a character in the entered text
-            Entry.delete(text_enter, 0) 
+    my_label_next = Label(master=Application_main.root, image=Application_main.bg)
+    my_label_next.place(x=0, y=0, relwidth=1, relheight=1)
+    my_label = my_label_next
+    my_label_next.destroy()
+    my_label.destroy()
+def handle_click_btnInsert(event): # Create function to handle btnInsert
 
-        def display(): # Function to display changed text
-            text = text_enter.get()
-            num_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-            text_update = ''
-            for i in range(len(text)):
-                if text[i] in num_list:
-                    text_update += '1,2,3'
-                else:
-                    text_update += text[i]
+    clear_main_label()
 
-            text_label['text'] = text_update
+    def clear(): # function to remove a character in the entered text
+        Entry.delete(text_enter, 0)
 
-        def exitInsert():
-            pass
+    def display(): # Function to display changed text
+        text = text_enter.get()
+        num_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        text_update = ''
+        for i in range(len(text)):
+            if text[i] in num_list:
+                text_update += '1,2,3'
+            else:
+                text_update += text[i]
 
-        if Function.onClickInsert: # if the button is not pressed, the process starts
-            text_label = Label() # Create window for text
-            text_enter = Entry() # Create window to input text
-            text_label.pack(anchor=CENTER, padx=6, pady=6)
-            text_enter.pack(anchor=CENTER, padx=6, pady=6)
-                        
-            # Set parameters for buttons
-            display_click_btnInsert = Button(text='Display', command=display)
-            display_click_btnInsert.pack(anchor=CENTER, padx=6, pady=6)
+        text_label['text'] = text_update
 
-            clear_click_btnInsert = Button(text='Clear', command=clear)
-            clear_click_btnInsert.pack(anchor=CENTER, padx=6, pady=6)
-            
-            Function.onClickInsert = False
+    def exitInsert():
+        pass
+
+    # Set parameters for buttons
+    display_click_btnInsert = Button(text='Display', command=display)
+    display_click_btnInsert.pack(anchor=CENTER, padx=6, pady=6)
+
+    clear_click_btnInsert = Button(text='Clear', command=clear)
+    clear_click_btnInsert.pack(anchor=CENTER, padx=6, pady=6)
+
 
     text_label = Label(height=20,width=100,anchor = 'e') # Create window for text
     text_enter = Entry(justify= tkinter.CENTER,width=50) # Create window to input text
@@ -69,25 +66,17 @@ def create_buttons_themes():
     btnSocial.place(x = 200,y =60)
     btnTechnology.place(x = 200,y =90)
 
-class Home(Helper):
-    onClickHome = True
 
-    def __init__(self, root_out, onClickHome):
-        super().__init__(root_out)
-        self.onClickHome = onClickHome
+def handle_click_btnHome(event): # Create function to handle btnHome
+    with open('home.txt', 'r') as file:
+        home_text = file.read() # Open and read file home.txt
+    try:
+        home_label = Label(text=home_text, anchor='n')
+        home_label_btn_close = Button(home_label, text='Закрыть', anchor='s', command=home_label.destroy)
+        # home_label.grid(row=100, column=59)
+        home_label.pack()
+        home_label_btn_close.pack(padx=200, pady=100)
 
-    def handle_click_btnHome(event): # Create function to handle btnHome
-        if Home.onClickHome: # if the button is not pressed, the process starts
-            with open('home.txt', 'r') as file:
-                home_text = file.read() # Open and read file home.txt
-            try:
-                home_label = Label(text=home_text, anchor='n')
-                home_label_btn_close = Button(home_label, text='Закрыть', anchor='s', command=home_label.destroy)
-                # home_label.grid(row=100, column=59)
-                home_label.pack()
-                home_label_btn_close.pack(padx=200, pady=100)
-                Home.onClickHome = False
-                    
-            except Tk.report_callback_exception as ex:
-                print('Error!', ex)
+    except Tk.report_callback_exception as ex:
+        print('Error!', ex)
 
